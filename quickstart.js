3cf7d6fbe7ed746374727eb48d70fd1195f08c33
akkidg <akashg@zabuzalabs.com>
2017-03-08 15:12:03 +0530

    curly brackets added

--- quickstart.js
+++ quickstart.js
@@ -411,24 +411,26 @@ function generateTemplateObject(jsonObject){
   var mainObject = jsonObject.main;
   var windObject = jsonObject.wind;
 
-  var json = attachment:{
-            type: "template",
-            payload: {
-              template_type: "generic",
-              elements: [{
-                title: "Weather Forecast for " + jsonObject.name + " will be " + weatherObjectArray[0].description,
-                subtitle:"Temp. : " + mainObject.temp + "\n Wind Speed: " + windObject.speed  + "\n Humidity: " + mainObject.humidity,
-                item_url: "",               
-                image_url: "http://openweathermap.org/img/w/" + weatherObjectArray[0].icon + ".png",
-                buttons: [{
-                  type: "postback",
-                  payload: "DEVELOPER_DEFINED_PAYLOAD_FOR_PARTY_SPECIAL_BACK",
-                  title: "Back"
-                }],
-              }]
-            }
-        };
-
+  var json = {
+              attachment:{
+                type: "template",
+                payload: {
+                  template_type: "generic",
+                  elements: [{
+                    title: "Weather Forecast for " + jsonObject.name + " will be " + weatherObjectArray[0].description,
+                    subtitle:"Temp. : " + mainObject.temp + "\n Wind Speed: " + windObject.speed  + "\n Humidity: " + mainObject.humidity,
+                    item_url: "",               
+                    image_url: "http://openweathermap.org/img/w/" + weatherObjectArray[0].icon + ".png",
+                    buttons: [{
+                      type: "postback",
+                      payload: "DEVELOPER_DEFINED_PAYLOAD_FOR_PARTY_SPECIAL_BACK",
+                      title: "Back"
+                    }],
+                  }]
+                  }
+                }
+                };
+  
   return JSON.stringify(json);   
 
 }
